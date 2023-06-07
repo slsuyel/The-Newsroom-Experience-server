32effceb537bdf7fc3213607916d093a6d5b79d6
Suyel Haque <slsuyel@gmail.com>
2023-06-07 17:05:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://schoolDb:jhuorUxtvBrLMt4w@cluster0.37kn8jw.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection = client.db("schoolDb").collection("users");
+    const selectedClassCollection = client
+      .db("schoolDb")
+      .collection("selectedClass");
+    const allClassCollection = client.db("schoolDb").collection("allClass");
+
+    /* User  */
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      return res.send(result);
+    });
+
+    /* All class */
+    app.get("/classes", async (req, res) => {
+      const data = await allClassCollection.find({}).toArray();
+      return res.send(data);
+    });
+
+    app.post("/selectedClasses", async (req, res) => {
+      const data = req.body;
+      const result = await selectedClassCollection.insertOne(data);
+      return res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("School is open now");
+});
+
+app.listen(port, () => {
+  console.log(`School is open on port ${port}`);
+});
