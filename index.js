dc66fbacaae5003f826594558eb1875d3cd4c874
Suyel Haque <slsuyel@gmail.com>
2023-06-09 09:33:55 +0600

    delete class by admin

--- index.js
+++ index.js
@@ -110,7 +110,16 @@ async function run() {
         res.send(result);
       }
     });
-    
+    /* Admin */
+
+    app.delete("/addclass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addClassCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
     /* - - --- ------ */
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
