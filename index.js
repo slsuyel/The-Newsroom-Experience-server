ab124ffb91b81e3b356270a9b2b3d4ddbd620b04
Suyel Haque <slsuyel@gmail.com>
2023-06-11 13:40:13 +0600

    env file added

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  "mongodb+srv://schoolDb:jhuorUxtvBrLMt4w@cluster0.37kn8jw.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.37kn8jw.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
