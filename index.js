91ac1286dfc5f957dc79544fdeffa41496bef999
Suyel Haque <slsuyel@gmail.com>
2023-06-12 14:14:58 +0600

    stripe

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const stripe = require("stripe")(process.env.PAYMENT_STRIPE_SK)
+
 // middleware
 app.use(cors());
 app.use(express.json());
